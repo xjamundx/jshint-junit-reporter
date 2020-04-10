82bac494955372d3ba3db462857584482be20e21
András Fördos <andras.fordos@avanade.com>
2020-04-10 16:59:06 +0200

    Fix broken tests to reflect latest reporter

--- test.js
+++ test.js
@@ -1,82 +1,115 @@
 var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
+
 var outputFile = __dirname + '/test.xml';
 var tests = [];
-var mock1 = []; // 0 error
-var mock2 = [{  // 1 error
-  file: './my/file.js',
-  error: {
-      id: '(error)',
-      raw: 'Missing semicolon.',
-      code: 'W033',
-      evidence: '}',
-      line: 36,
-      character: 2,
-      scope: '(main)',
-      a: undefined,
-      b: undefined,
-      c: undefined,
-      d: undefined,
-      reason: 'Missing semicolon.'
-  }
-}];
-var mock3 = [ { file: 'reporter.js',
-    error: 
-     { id: '(error)',
-       raw: 'Missing "use strict" statement.',
-       evidence: '  var pairs = {',
-       line: 9,
-       character: 3,
-       scope: '(main)',
-       a: undefined,
-       b: undefined,
-       c: undefined,
-       d: undefined,
-       reason: 'Missing "use strict" statement.' } },
-  { file: 'reporter.js',
-    error: 
-     { id: '(error)',
-       raw: 'Missing "use strict" statement.',
-       evidence: '  var count = failures.length;',
-       line: 25,
-       character: 3,
-       scope: '(main)',
-       a: undefined,
-       b: undefined,
-       c: undefined,
-       d: undefined,
-       reason: 'Missing "use strict" statement.' } },
-  { file: 'reporter.js',
-    error: 
-     { id: '(error)',
-       raw: 'Missing "use strict" statement.',
-       evidence: '  var msg = [];',
-       line: 34,
-       character: 3,
-       scope: '(main)',
-       a: undefined,
-       b: undefined,
-       c: undefined,
-       d: undefined,
-       reason: 'Missing "use strict" statement.' } },
-  { file: 'reporter.js',
-    error: 
-     { id: '(error)',
-       raw: 'Missing "use strict" statement.',
-       evidence: '  console.log(results)',
-       line: 45,
-       character: 3,
-       scope: '(main)',
-       a: undefined,
-       b: undefined,
-       c: undefined,
-       d: undefined,
-       reason: 'Missing "use strict" statement.' } } ];
-var expected1 = '';
+// 0 error
+var mock1 = {
+  results: [],
+  data: [],
+  opts: null
+};
+// 1 error
+var mock2 = {
+  results: [{
+    file: './my/file.js',
+    error: {
+        id: '(error)',
+        raw: 'Missing semicolon.',
+        code: 'W033',
+        evidence: '}',
+        line: 36,
+        character: 2,
+        scope: '(main)',
+        a: undefined,
+        b: undefined,
+        c: undefined,
+        d: undefined,
+        reason: 'Missing semicolon.'
+    }
+  }],
+  data: [{
+    file: 'my/file.js'
+  }],
+  opts: null
+};
+// 4 errors
+var mock3 = {
+  results: [
+    { 
+      file: 'reporter.js',
+      error: {
+        id: '(error)',
+        raw: 'Missing "use strict" statement.',
+        evidence: '  var pairs = {',
+        line: 9,
+        character: 3,
+        scope: '(main)',
+        a: undefined,
+        b: undefined,
+        c: undefined,
+        d: undefined,
+        reason: 'Missing "use strict" statement.' 
+      }
+    },
+    { 
+      file: 'reporter.js',
+      error: { 
+        id: '(error)',
+        raw: 'Missing "use strict" statement.',
+        evidence: '  var count = failures.length;',
+        line: 25,
+        character: 3,
+        scope: '(main)',
+        a: undefined,
+        b: undefined,
+        c: undefined,
+        d: undefined,
+        reason: 'Missing "use strict" statement.' 
+      }
+    },
+    { 
+      file: 'reporter.js',
+      error: { 
+        id: '(error)',
+        raw: 'Missing "use strict" statement.',
+        evidence: '  var msg = [];',
+        line: 34,
+        character: 3,
+        scope: '(main)',
+        a: undefined,
+        b: undefined,
+        c: undefined,
+        d: undefined,
+        reason: 'Missing "use strict" statement.' 
+      }
+    },
+    { 
+      file: 'reporter.js',
+      error: { 
+        id: '(error)',
+        raw: 'Missing "use strict" statement.',
+        evidence: '  console.log(results)',
+        line: 45,
+        character: 3,
+        scope: '(main)',
+        a: undefined,
+        b: undefined,
+        c: undefined,
+        d: undefined,
+        reason: 'Missing "use strict" statement.' 
+      }
+    }
+  ],
+  data: [{
+    file: 'reporter.js'
+  }],
+  opts: null
+};
 var expected2 = '<?xml version="1.0" encoding="utf-8"?><testsuite name="jshint" tests="1" failures="1" errors="0"><testcase name="my/file.js"><failure message="1 JSHINT Failure">1. line 36, char 2: Missing semicolon.</failure></testcase></testsuite>';
-var expected3 = '<?xml version="1.0" encoding="utf-8"?><testsuite name="jshint" tests="1" failures="4" errors="0"><testcase name="reporter.js"><failure message="4 JSHint Failures">1. line 9, char 3: Missing &quot;use strict&quot; statement. 2. line 25, char 3: Missing &quot;use strict&quot; statement. 3. line 34, char 3: Missing &quot;use strict&quot; statement. 4. line 45, char 3: Missing &quot;use strict&quot; statement.</failure></testcase></testsuite>';
 var expected3 = '<?xml version="1.0" encoding="utf-8"?><testsuite name="jshint" tests="1" failures="4" errors="0"><testcase name="reporter.js"><failure message="4 JSHint Failures">1. line 9, char 3: Missing &quot;use strict&quot; statement.2. line 25, char 3: Missing &quot;use strict&quot; statement.3. line 34, char 3: Missing &quot;use strict&quot; statement.4. line 45, char 3: Missing &quot;use strict&quot; statement.</failure></testcase></testsuite>';
+
 var strip = new RegExp('[\t\n]', 'g');
 var oldWrite = process.stdout.write.bind(process.stdout);
 
@@ -92,12 +125,11 @@ tests.push(function() {
   var formatter = require('./reporter.js');
 });
 
-
 // test output for 0 errors
 tests.push(function() {
   var formatter = require('./reporter.js');
   process.stdout.write = function() {};
-  var results = formatter.reporter(mock1);
+  var results = formatter.reporter(mock1.results, mock1.data, mock1.opts);
   process.stdout.write = oldWrite;
   var expected = ['<?xml', '<testcase', '<testsuite'];
   if (!results) throw new Error('You should have some XML!');
@@ -112,7 +144,7 @@ tests.push(function() {
 tests.push(function() {
   process.stdout.write = function() {};
   var formatter = require('./reporter.js');
-  var results = formatter.reporter(mock2);
+  var results = formatter.reporter(mock2.results, mock2.data, mock2.opts);
   process.stdout.write = oldWrite;
   if (results.replace(strip, '') !== expected2.replace(strip, '')) {
     throw new Error('Unexpected results');
@@ -123,7 +155,7 @@ tests.push(function() {
 tests.push(function() {
   process.stdout.write = function() {};
   var formatter = require('./reporter.js');
-  var results = formatter.reporter(mock3);
+  var results = formatter.reporter(mock3.results, mock3.data, mock3.opts);
   process.stdout.write = oldWrite;
   if (results.replace(strip, '') !== expected3.replace(strip, '')) {
     throw new Error('Unexpected results');
@@ -133,7 +165,7 @@ tests.push(function() {
 // test for output file
 tests.push(function() {
   var formatter = require('./reporter.js');
-  var results = formatter.reporter(mock3, null, { outputFile: outputFile });
+  var results = formatter.reporter(mock3.results, mock3.data, { outputFile: outputFile });
   var success = false;
   var content;
   if (fs.existsSync(outputFile)) {
@@ -146,7 +178,6 @@ tests.push(function() {
     throw new Error('Unexpected results');
   }
 });
-
 
 // lint this file, but only if you have jshint
 tests.push(function() {
